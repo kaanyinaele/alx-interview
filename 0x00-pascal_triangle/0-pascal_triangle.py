08faaaa4f30081f8806135cd6c4b02fd0dc01304
kaanyinaele <kaanyinaele@gmail.com>
2024-07-26 13:12:13 +0100

    Pascal trainagle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
+
+
 def pascal_triangle(n):
+    """ returns pascal triangle
+    """
     if n <= 0:
         return []
 
-    triangle = []
-    
-    for i in range(n):
-        # Start each row with 1
-        row = [1] * (i + 1)
-        
-        # Each triangle row (except the first and last element) is the sum of the two above it
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        
-        triangle.append(row)
-    
-    return triangle
+    Ptriangle = []
 
+    for i in range(n):
+        # first element
+        my_List = [1]
+        if i == 0:
+            Ptriangle.append(my_List)
+            continue
+
+        k = i - 1
+        for j in range(len(Ptriangle[k])): 
+            if j + 1 == len(Ptriangle[k]):
+                # last element
+                my_List.append(1)
+                break
+            # Add two previous values to get current next value
+            nextVal = Ptriangle[k][j] + Ptriangle[k][j + 1]
+            my_List.append(nextVal)
+        Ptriangle.append(my_List)
+
+    return Ptriangle
