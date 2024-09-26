515bc6cf1203916d9cf7b5e8a118f5e13f744b93
kaanyinaele <kaanyinaele@gmail.com>
2024-09-26 09:06:56 +0100

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,57 +1,26 @@
 #!/usr/bin/python3
 
+"""Island perimeter computing module.
+"""
+
+
 def island_perimeter(grid):
-    """ Get the dimensions of the grid
-    """
-    rows, cols = len(grid), len(grid[0])
-    
-    """ Create a visited matrix to keep track of explored cells
-    """
-    visited = [[False] * cols for _ in range(rows)]
-    
-    """ Initialize perimeter counter
+    """Computes the perimeter of an island with no lakes.
     """
     perimeter = 0
-    
-    def bfs(row, col):
-        """Check if the current cell is out of bounds or water.
-        """
-        if row < 0 or row >= rows or col < 0 or col >= cols or grid[row][col] == 0 or visited[row][col]:
-            return True
-        
-        """ Mark the cell as visited
-        """
-        visited[row][col] = True
-        
-        """ Define all possible directions to explore
-        """
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        
-        """ Explore neighboring cells
-        """
-        for dr, dc in directions:
-            new_row, new_col = row + dr, col + dc
-            if bfs(new_row, new_col):
-                return True
-        
-        """ If no water cell found, return False
-        """
-        return False
-    
-    """ Traverse the grid and calculate perimeter
-    """
-    for r in range(rows):
-        for c in range(cols):
-            """ Check if current cell is land and not visited
-            """
-            if grid[r][c] == 1 and not visited[r][c]:
-                """ Start BFS traversal from this cell
-                """
-                if bfs(r, c):
-                    """Increment perimeter if BFS returns True
-                    """
-                    perimeter += 1
-    
-    """ Return the total calculated perimeter
-    """
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
